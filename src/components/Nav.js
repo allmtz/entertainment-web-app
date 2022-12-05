0c0544400ac357ddb9edadf058bb9eb5eb1e13c3
Allan <martinezallan123@gmail.com>
2022-12-04 18:12:02 -0800

    removed home icon + prettier

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,31 +1,32 @@
+import { ReactComponent as MovieIcon } from "../assets/icon-nav-movies.svg";
+import { ReactComponent as TvIcon } from "../assets/icon-nav-tv-series.svg";
+import { ReactComponent as BookmarkIconNav } from "../assets/icon-nav-bookmark.svg";
+import { Link } from "react-router-dom";
 
-import {ReactComponent as HomeIcon} from '../assets/icon-nav-home.svg'  
-import {ReactComponent as MovieIcon} from '../assets/icon-nav-movies.svg'  
-import {ReactComponent as TvIcon} from '../assets/icon-nav-tv-series.svg'  
-import {ReactComponent as BookmarkIconNav} from '../assets/icon-nav-bookmark.svg'  
-import { Link } from 'react-router-dom'
-
-export const Nav = ( ) => {
-
-    return(
+export const Nav = () => {
+  return (
     <nav>
-        <Link to="/">
-          <img className="site-logo" src="/assets/logo.svg" alt="site logo" />
-        </Link>
-        <ul>
+      <Link to="/">
+        <img className="site-logo" src="/assets/logo.svg" alt="site logo" />
+      </Link>
+      <ul>
+        <li>
           <Link to="/">
-            <li><HomeIcon /></li>
+            <MovieIcon />
           </Link>
-          <li><MovieIcon /></li>
+        </li>
+        <li>
           <Link to="/tv">
-            <li><TvIcon /></li>
+            <TvIcon />
           </Link>
+        </li>
+        <li>
           <Link to="/bookmarks">
-            <li><BookmarkIconNav /></li>
+            <BookmarkIconNav />
           </Link>
-        </ul>
-
-        <img className="avatar" src="/assets/image-avatar.png" alt="avatar" />
-      </nav>
-    )
-}
+        </li>
+      </ul>
+      <img className="avatar" src="/assets/image-avatar.png" alt="avatar" />
+    </nav>
+  );
+};
