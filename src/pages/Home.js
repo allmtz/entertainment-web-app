104b4860eaee3aa649932c71b9204634f354f9cc
Allan <martinezallan123@gmail.com>
2022-12-04 18:27:18 -0800

    deleted <Landing>

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,16 @@
-import { Landing } from "../components/Landing"
+import { Carousel } from "../components/Carousel"
 
 export const Home = ({ trending, getInfo, action, western, comedy, animation, romance, drama, fantasy, favorite, isFavorite, mediaType }) => {
     return(
         <>
-            <Landing trending={trending} getInfo={getInfo} action={action} western={western} comedy={comedy} animation={animation} romance={romance} drama={drama} fantasy={fantasy} favorite={favorite} isFavorite={isFavorite} mediaType={mediaType} /> 
+        <Carousel getInfo={getInfo} genre={"Trending"} genreMovies={trending} favorite={favorite} isFavorite={isFavorite} mediaType={mediaType} />
+        <Carousel getInfo={getInfo} genre={"Action"} genreMovies={action}  favorite={favorite} isFavorite={isFavorite} mediaType={mediaType} />
+        <Carousel getInfo={getInfo} genre={"Western"} genreMovies={western} favorite={favorite} isFavorite={isFavorite} mediaType={mediaType} />
+        <Carousel getInfo={getInfo} genre={"Comedy"} genreMovies={comedy} favorite={favorite} isFavorite={isFavorite} mediaType={mediaType} />
+        <Carousel getInfo={getInfo} genre={"Animation"} genreMovies={animation} favorite={favorite} isFavorite={isFavorite} mediaType={mediaType} />
+        <Carousel getInfo={getInfo} genre={"Romance"} genreMovies={romance} favorite={favorite} isFavorite={isFavorite} mediaType={mediaType} />
+        <Carousel getInfo={getInfo} genre={"Drama"} genreMovies={drama} favorite={favorite} isFavorite={isFavorite} mediaType={mediaType} />
+        <Carousel getInfo={getInfo} genre={"Fantasy"} genreMovies={fantasy} favorite={favorite} isFavorite={isFavorite} mediaType={mediaType}  /> 
         </>
     )
 }
