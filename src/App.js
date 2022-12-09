1c4339223f2f47588e7de40c24458318495f5654
Allan <martinezallan123@gmail.com>
2022-12-09 15:21:29 -0800

    added star rating display + TMDB credit

--- src/App.js
+++ src/App.js
@@ -222,6 +222,10 @@ function App() {
             <Route path="/tv" element={ <Tv trendingTV={trendingTV} getInfo={getInfo} actionTV={actionTV} westernTV={westernTV} comedyTV={comedyTV} animationTV={animationTV} romanceTV={romanceTV} dramaTV={dramaTV} favorite={favorite} isFavorite={isFavorite} /> } />
           </Routes>
         </main>
+        <footer>
+          <p>Powered</p>
+          <img className="TMDBLogo" src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg" alt="TMDB logo" />
+        </footer>
       </div>
   );
 }
