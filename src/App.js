e5e3f5763c36c66b117e2d6a092b68de0a7cd15d
Allan <martinezallan123@gmail.com>
2022-12-09 15:24:23 -0800

    link to TMDB api

--- src/App.js
+++ src/App.js
@@ -223,8 +223,10 @@ function App() {
           </Routes>
         </main>
         <footer>
-          <p>Powered</p>
-          <img className="TMDBLogo" src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg" alt="TMDB logo" />
+          <p>Powered by</p>
+          <a href="https://www.themoviedb.org/documentation/api?language=en-US" target="_blank" rel="noreferrer">
+            <img className="TMDBLogo" src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg" alt="TMDB logo" />
+          </a>
         </footer>
       </div>
   );
