86877c2b52200563c7f9288cfe7d3075dcf11d1b
Allan <martinezallan123@gmail.com>
2022-12-10 12:11:10 -0800

    centering footer on large screens

--- src/App.js
+++ src/App.js
@@ -225,14 +225,14 @@ function App() {
             <Route path="/bookmarks" element={<Results moviesToDisplay={favorites} getInfo={getInfo} favorite={favorite} isFavorite={isFavorite} /> } />
             <Route path="/tv" element={ <Tv trendingTV={trendingTV} getInfo={getInfo} actionTV={actionTV} westernTV={westernTV} comedyTV={comedyTV} animationTV={animationTV} romanceTV={romanceTV} dramaTV={dramaTV} favorite={favorite} isFavorite={isFavorite} /> } />
           </Routes>
+          <footer>
+            <p>Powered by</p>
+            <a href="https://www.themoviedb.org/documentation/api?language=en-US" target="_blank" rel="noreferrer">
+              <img className="TMDBLogo" src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg" alt="TMDB logo" />
+            </a>
+          </footer>
         </main>
       </div>
-      <footer>
-          <p>Powered by</p>
-          <a href="https://www.themoviedb.org/documentation/api?language=en-US" target="_blank" rel="noreferrer">
-            <img className="TMDBLogo" src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg" alt="TMDB logo" />
-          </a>
-        </footer>
       </>
   );
 }
